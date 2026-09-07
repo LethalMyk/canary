65d5febfbb06a0dc1b7d5e7cd1f1c7e8693aebcc
Mayke Fleury <mayke.fleury@gmail.com>
2026-09-07 05:15:38 -0300

    fix look

--- data/scripts/eventcallbacks/player/on_look.lua
+++ data/scripts/eventcallbacks/player/on_look.lua
@@ -11,6 +11,7 @@ local function isSpecialItem(itemId)
 			return true
 		end
 	end
+
 	return false
 end
 
@@ -18,7 +19,12 @@ local function getPositionDescription(position)
 	if position.x == 65535 then
 		return "Position: In your inventory."
 	else
-		return string.format("Position: (%d, %d, %d)", position.x, position.y, position.z)
+		return string.format(
+			"Position: (%d, %d, %d)",
+			position.x,
+			position.y,
+			position.z
+		)
 	end
 end
 
@@ -26,16 +32,27 @@ local function handleItemDescription(inspectedThing, lookDistance, player)
 	local descriptionText = inspectedThing:getDescription(lookDistance)
 
 	if not player:getGroup():getAccess() then
-		if inspectedThing:getId() == ITEM_MAGICWALL or inspectedThing:getId() == ITEM_MAGICWALL_SAFE then
+		if inspectedThing:getId() == ITEM_MAGICWALL
+			or inspectedThing:getId() == ITEM_MAGICWALL_SAFE then
+
 			return "You see a magic wall."
-		elseif inspectedThing:getId() == ITEM_WILDGROWTH or inspectedThing:getId() == ITEM_WILDGROWTH_SAFE then
+
+		elseif inspectedThing:getId() == ITEM_WILDGROWTH
+			or inspectedThing:getId() == ITEM_WILDGROWTH_SAFE then
+
 			return "You see rush wood."
 		end
 	end
-	if isSpecialItem(inspectedThing.itemid) then
+
+	if isSpecialItem(inspectedThing:getId()) then
 		local itemCharges = inspectedThing:getCharges()
+
 		if itemCharges > 0 then
-			return string.format("You see %s\nIt has %d refillings left.", descriptionText, itemCharges)
+			return string.format(
+				"You see %s\nIt has %d refillings left.",
+				descriptionText,
+				itemCharges
+			)
 		end
 	else
 		return "You see " .. descriptionText
@@ -49,9 +66,21 @@ local function handleCreatureDescription(inspectedThing, lookDistance)
 
 	if inspectedThing:isMonster() then
 		local monsterMaster = inspectedThing:getMaster()
-		if monsterMaster and table.contains(FAMILIARSNAME, inspectedThing:getName():lower()) then
-			local summonTimeRemaining = monsterMaster:kv():get("familiar-summon-time") or 0
-			descriptionText = string.format("%s (Master: %s). It will disappear in %s", descriptionText, monsterMaster:getName(), Game.getTimeInWords(summonTimeRemaining - os.time()))
+
+		if monsterMaster
+			and table.contains(FAMILIARSNAME, inspectedThing:getName():lower()) then
+
+			local summonTimeRemaining =
+				monsterMaster:kv():get("familiar-summon-time") or 0
+
+			descriptionText = string.format(
+				"%s (Master: %s). It will disappear in %s",
+				descriptionText,
+				monsterMaster:getName(),
+				Game.getTimeInWords(
+					summonTimeRemaining - os.time()
+				)
+			)
 		end
 	end
 
@@ -60,60 +89,143 @@ end
 
 local function appendAdminDetails(descriptionText, inspectedThing, inspectedPosition)
 	if inspectedThing:isItem() then
-		descriptionText = string.format("%s\nClient ID: %d", descriptionText, inspectedThing:getId())
+		descriptionText = string.format(
+			"%s\nClient ID: %d",
+			descriptionText,
+			inspectedThing:getId()
+		)
 
 		local itemActionId = inspectedThing:getActionId()
+
 		if itemActionId ~= 0 then
-			descriptionText = string.format("%s, Action ID: %d", descriptionText, itemActionId)
+			descriptionText = string.format(
+				"%s, Action ID: %d",
+				descriptionText,
+				itemActionId
+			)
 		end
 
 		local itemUniqueId = inspectedThing:getUniqueId()
+
 		if itemUniqueId > 0 and itemUniqueId < 65536 then
-			descriptionText = string.format("%s, Unique ID: %d", descriptionText, itemUniqueId)
+			descriptionText = string.format(
+				"%s, Unique ID: %d",
+				descriptionText,
+				itemUniqueId
+			)
 		end
 
-		local doorIdAttribute = inspectedThing:getAttribute(ITEM_ATTRIBUTE_DOORID)
+		local doorIdAttribute =
+			inspectedThing:getAttribute(ITEM_ATTRIBUTE_DOORID)
+
 		if doorIdAttribute and doorIdAttribute > 0 then
-			descriptionText = string.format("%s, Door ID: %d", descriptionText, doorIdAttribute)
+			descriptionText = string.format(
+				"%s, Door ID: %d",
+				descriptionText,
+				doorIdAttribute
+			)
 		end
 
 		local itemType = inspectedThing:getType()
-		local transformOnEquipId = itemType:getTransformEquipId()
-		local transformOnDeEquipId = itemType:getTransformDeEquipId()
+
+		local transformOnEquipId =
+			itemType:getTransformEquipId()
+
+		local transformOnDeEquipId =
+			itemType:getTransformDeEquipId()
 
 		if transformOnEquipId ~= 0 then
-			descriptionText = string.format("%s\nTransforms to: %d (onEquip)", descriptionText, transformOnEquipId)
+			descriptionText = string.format(
+				"%s\nTransforms to: %d (onEquip)",
+				descriptionText,
+				transformOnEquipId
+			)
+
 		elseif transformOnDeEquipId ~= 0 then
-			descriptionText = string.format("%s\nTransforms to: %d (onDeEquip)", descriptionText, transformOnDeEquipId)
+			descriptionText = string.format(
+				"%s\nTransforms to: %d (onDeEquip)",
+				descriptionText,
+				transformOnDeEquipId
+			)
 		end
 
 		local itemDecayId = itemType:getDecayId()
+
 		if itemDecayId ~= -1 then
-			descriptionText = string.format("%s\nDecays to: %d", descriptionText, itemDecayId)
-		end
-	elseif inspectedThing:isCreature() then
-		local healthDescription, creatureId = "%s\n%s\nHealth: %d / %d"
-		if inspectedThing:isPlayer() and inspectedThing:getMaxMana() > 0 then
-			creatureId = string.format("Player ID: %i", inspectedThing:getGuid())
-			healthDescription = string.format("%s, Mana: %d / %d", healthDescription, inspectedThing:getMana(), inspectedThing:getMaxMana())
-		elseif inspectedThing:isMonster() then
-			creatureId = string.format("Monster ID: %i", inspectedThing:getId())
-		elseif inspectedThing:isNpc() then
-			creatureId = string.format("NPC ID: %i", inspectedThing:getId())
+			descriptionText = string.format(
+				"%s\nDecays to: %d",
+				descriptionText,
+				itemDecayId
+			)
 		end
 
-		descriptionText = string.format(healthDescription, descriptionText, creatureId, inspectedThing:getHealth(), inspectedThing:getMaxHealth())
+	elseif inspectedThing:isCreature() then
+
+		local healthDescription = "%s\n%s\nHealth: %d / %d"
+		local creatureId = ""
+
+		if inspectedThing:isPlayer()
+			and inspectedThing:getMaxMana() > 0 then
+
+			creatureId = string.format(
+				"Player ID: %i",
+				inspectedThing:getGuid()
+			)
+
+			healthDescription = string.format(
+				"%s, Mana: %d / %d",
+				healthDescription,
+				inspectedThing:getMana(),
+				inspectedThing:getMaxMana()
+			)
+
+		elseif inspectedThing:isMonster() then
+
+			creatureId = string.format(
+				"Monster ID: %i",
+				inspectedThing:getId()
+			)
+
+		elseif inspectedThing:isNpc() then
+
+			creatureId = string.format(
+				"NPC ID: %i",
+				inspectedThing:getId()
+			)
+		end
+
+		descriptionText = string.format(
+			healthDescription,
+			descriptionText,
+			creatureId,
+			inspectedThing:getHealth(),
+			inspectedThing:getMaxHealth()
+		)
 	end
 
-	descriptionText = string.format("%s\n%s", descriptionText, getPositionDescription(inspectedPosition))
+	descriptionText = string.format(
+		"%s\n%s",
+		descriptionText,
+		getPositionDescription(inspectedPosition)
+	)
 
 	if inspectedThing:isCreature() then
 		local creatureBaseSpeed = inspectedThing:getBaseSpeed()
 		local creatureCurrentSpeed = inspectedThing:getSpeed()
-		descriptionText = string.format("%s\nSpeed Base: %d\nSpeed: %d", descriptionText, creatureBaseSpeed, creatureCurrentSpeed)
+
+		descriptionText = string.format(
+			"%s\nSpeed Base: %d\nSpeed: %d",
+			descriptionText,
+			creatureBaseSpeed,
+			creatureCurrentSpeed
+		)
 
 		if inspectedThing:isPlayer() then
-			descriptionText = string.format("%s\nIP: %s", descriptionText, Game.convertIpToString(inspectedThing:getIp()))
+			descriptionText = string.format(
+				"%s\nIP: %s",
+				descriptionText,
+				Game.convertIpToString(inspectedThing:getIp())
+			)
 		end
 	end
 
@@ -122,40 +234,110 @@ end
 
 local callback = EventCallback("PlayerOnLookBaseEvent")
 
-function callback.playerOnLook(player, inspectedThing, inspectedPosition, lookDistance)
-	local descriptionText
+function callback.playerOnLook(
+	player,
+	inspectedThing,
+	inspectedPosition,
+	lookDistance
+)
+	local descriptionText = ""
 
-
-	
-		-- Look KILL AND DEATH --
-		if inspectedThing:isPlayer() and not inspectedThing:getGroup():getAccess() then
-			local killStorage = 884734
-			local deathStorage = 884735
-			local resetStorage = 500
-			local FightPoints = Storage.PvpFight.Points
-			local killAmount, deathAmount, resetAmount, FPoints = inspectedThing:getStorageValue(killStorage), inspectedThing:getStorageValue(deathStorage), inspectedThing:getStorageValue(resetStorage), inspectedThing	:getStorageValue(FightPoints)
-			if killAmount == -1 then killAmount = 0 end
-			if deathAmount == -1 then deathAmount = 0 end
-			if resetAmount == -1 then resetAmount = 0 end
-			if FPoints == -1 then FPoints = 0 end
-
-
-
-			description = description .. '\nKilleds: [' ..killAmount..'] and ' .. 'Deaths: ['..deathAmount..']' .. '\nResets: ['..resetAmount..']' .. '\nFight Points: ['..FPoints..']'
-		end
-		
+	-- =========================================================
+	-- DESCRIÇÃO DO OBJETO
+	-- =========================================================
 
 	if inspectedThing:isItem() then
-		descriptionText = handleItemDescription(inspectedThing, lookDistance, player)
+
+		descriptionText = handleItemDescription(
+			inspectedThing,
+			lookDistance,
+			player
+		)
+
 	elseif inspectedThing:isCreature() then
-		descriptionText = handleCreatureDescription(inspectedThing, lookDistance)
+
+		descriptionText = handleCreatureDescription(
+			inspectedThing,
+			lookDistance
+		)
 	end
+
+	-- =========================================================
+	-- KILLS / DEATHS / RESETS / FIGHT POINTS
+	-- =========================================================
+
+	if inspectedThing:isPlayer()
+		and not inspectedThing:getGroup():getAccess() then
+
+		local killStorage = 884734
+		local deathStorage = 884735
+		local resetStorage = 500
+		local fightPointsStorage = Storage.PvpFight.Points
+
+		local killAmount =
+			inspectedThing:getStorageValue(killStorage)
+
+		local deathAmount =
+			inspectedThing:getStorageValue(deathStorage)
+
+		local resetAmount =
+			inspectedThing:getStorageValue(resetStorage)
+
+		local fightPoints =
+			inspectedThing:getStorageValue(fightPointsStorage)
+
+		-- Storage não inicializado
+		if killAmount < 0 then
+			killAmount = 0
+		end
+
+		if deathAmount < 0 then
+			deathAmount = 0
+		end
+
+		if resetAmount < 0 then
+			resetAmount = 0
+		end
+
+		if fightPoints < 0 then
+			fightPoints = 0
+		end
+
+		-- Adiciona as informações à descrição
+		descriptionText = descriptionText
+			.. "\nKilleds: ["
+			.. killAmount
+			.. "] and Deaths: ["
+			.. deathAmount
+			.. "]"
+			.. "\nResets: ["
+			.. resetAmount
+			.. "]"
+			.. "\nFight Points: ["
+			.. fightPoints
+			.. "]"
+	end
+
+	-- =========================================================
+	-- INFORMAÇÕES DE ADMIN
+	-- =========================================================
 
 	if player:getGroup():getAccess() then
-		descriptionText = appendAdminDetails(descriptionText, inspectedThing, inspectedPosition)
+		descriptionText = appendAdminDetails(
+			descriptionText,
+			inspectedThing,
+			inspectedPosition
+		)
 	end
 
-	player:sendTextMessage(MESSAGE_LOOK, descriptionText)
+	-- =========================================================
+	-- ENVIA A DESCRIÇÃO
+	-- =========================================================
+
+	player:sendTextMessage(
+		MESSAGE_LOOK,
+		descriptionText
+	)
 end
 
 callback:register()
